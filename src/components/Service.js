9bd1c9d18baed836d6489d033f3e47c81b2c3374
prate <prateekprakash11@gmail.con>
2024-04-04 23:39:36 +0530

    added

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,193 +1,228 @@
 import React from "react";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea, IconButton } from '@mui/material';
-import appplincesrepair from '../images/appliance-repair (1).png';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from 'react-responsive-carousel';  
+import { Card, CardActionArea, Typography, IconButton } from '@mui/material';
 import ArrowRightAltSharpIcon from '@mui/icons-material/ArrowRightAltSharp';
-import {useRef} from 'react';
-import home from '../images/home.png'
+import appliancesrepair from '../images/appliance-repair (1).png';
+import home from "../images/home.png";
+import Grid from '@mui/material/Unstable_Grid2';
 import Paper from '@mui/material/Paper';
-import Stack from '@mui/material/Stack';
-import { styled } from '@mui/material/styles';
-import fitness from '../images/fitness.png';
-import advisors from "../images/quality-service.png";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import { Box, Container } from '@mui/material';
-
-const Service=()=>{
-     
-
-
-
-
-
-
-    return(
-
-
-      <>
-   
-        
-
-        <Card sx={{
-            maxWidth: 174,
-            borderRadius: 4,
-            boxShadow: '0px 8px 24px rgba(0, 0, 0, 0.5)',
-            margin: 'auto',
-            marginTop: 20,
-            
-            display: 'flex',
-            justifyContent: 'center', // Center the content horizontally
-            alignItems: 'center', // Center the content vertically
-        }}>
-            <CardActionArea  sx={{ marginLeft:"0px"}}>
-                <img
-                    src={appplincesrepair} // Assuming 'appplincesrepair' is your image URL
-                    alt="AC Repairing"
-                    style={{
-                        marginTop:20,
-                        marginLeft: 'auto', // Center the image horizontally
-                        marginRight: 'auto',
-                        display: 'block',
-                        objectFit: 'cover',
-                        maxWidth: '80%', // Limit image size to its container
-                        maxHeight: '100px', // Maximum height set to 200px
-                    }}
-                />
-                <CardContent  sx={{ paddingBottom: '1px' ,marginLeft:"0px"}}>
-                <Typography gutterBottom variant="h6" component="div" style={{ fontSize: '0.8rem', color: '#333', textAlign: 'left', fontWeight: 'bold', marginBottom: '1px' }}>
-                    Appliances Installation
-                    <Typography gutterBottom variant="h6" component="div" style={{ fontSize: '0.8rem', color: '#333', fontWeight: 'bold', textAlign: 'center', marginLeft:"20px" }}>
-                    & Repair
-                    <IconButton color="blue" sx={{ fontSize: 30, textAlign: 'center',}}>
-                    <ArrowRightAltSharpIcon />
-                </IconButton>
-                </Typography>
-                </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
-        <Card sx={{
-            maxWidth: 174,
-            borderRadius: 4,
-            boxShadow: '0px 8px 24px rgba(0, 0, 0, 0.5)',
-            margin: 'auto',
-            marginTop: 20,
-            display: 'flex',
-            justifyContent: 'center', // Center the content horizontally
-            alignItems: 'center', // Center the content vertically
-        }}>
-            <CardActionArea>
-                <img
-                    src={home} // Assuming 'appplincesrepair' is your image URL
-                    alt="AC Repairing"
-                    style={{
-                        marginTop:20,
-                        marginLeft: 'auto', // Center the image horizontally
-                        marginRight: 'auto',
-                        display: 'block',
-                        objectFit: 'cover',
-                        maxWidth: '80%', // Limit image size to its container
-                        maxHeight: '100px', // Maximum height set to 200px
-                    }}
-                />
-                <CardContent  sx={{ paddingBottom: '1px' }}>
-                <Typography gutterBottom variant="h6" component="div" style={{ fontSize: '0.8rem', color: '#333', textAlign: 'center', fontWeight: 'bold',  }}>
-                    House Cleaning
-                    <Typography gutterBottom variant="h6" component="div" style={{ fontSize: '0.8rem', color: '#333', fontWeight: 'bold', textAlign: 'center', marginLeft:'10px' }}>
-                    & Maintainance
-                    <IconButton color="blue" sx={{ fontSize: 30, textAlign: 'center',}}>
-                    <ArrowRightAltSharpIcon />
-                </IconButton>
-                </Typography>
-                </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
-        <Card sx={{
-            maxWidth: 174,
-            borderRadius: 4,
-            boxShadow: '0px 8px 24px rgba(0, 0, 0, 0.5)',
-            margin: 'auto',
-            marginTop: 20,
-        
-            display: 'flex',
-            justifyContent: 'center', // Center the content horizontally
-            alignItems: 'center', // Center the content vertically
-        }}>
-            <CardActionArea>
-                <img
-                    src={fitness} // Assuming 'appplincesrepair' is your image URL
-                    alt="AC Repairing"
-                    style={{
-                        marginTop:20,
-                        marginLeft: 'auto', // Center the image horizontally
-                        marginRight: 'auto',
-                        display: 'block',
-                        objectFit: 'cover',
-                        maxWidth: '80%', // Limit image size to its container
-                        maxHeight: '100px', // Maximum height set to 200px
-                    }}
-                />
-                <CardContent  sx={{ paddingBottom: '1px' }}>
-                <Typography gutterBottom variant="h6" component="div" style={{ fontSize: '0.8rem', color: '#333', textAlign: 'center', fontWeight: 'bold',  }}>
-                    Health
-                    <Typography gutterBottom variant="h6" component="div" style={{ fontSize: '0.8rem', color: '#333', fontWeight: 'bold', textAlign: 'center', marginLeft:'10px' }}>
-                    & Fitness
-                    <IconButton color="blue" sx={{ fontSize: 30, textAlign: 'center',}}>
-                    <ArrowRightAltSharpIcon />
-                </IconButton>
-                </Typography>
-                </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
+import Box from '@mui/material/Box';
+import appliancesinstall from"../images/outdoor-unit.png";
+import fitnesstrack from "../images/supplementary-food.png";
+import rennovate from "../images/renovation.png";
+import eventplanner from "../images/event-planner.png";
+import movers from "../images/mover-truck.png";
+import education from "../images/training.png"
+const Service = () => {
+    return (
+       <>
        
-        <Card sx={{
-            maxWidth: 174,
-            borderRadius: 4,
-            boxShadow: '0px 8px 24px rgba(0, 0, 0, 0.5)',
-            margin: 'auto',
-            marginTop: 20,
-        
-            display: 'flex',
-            justifyContent: 'center', // Center the content horizontally
-            alignItems: 'center', // Center the content vertically
-        }}>
-            <CardActionArea>
-                <img
-                    src={advisors} // Assuming 'appplincesrepair' is your image URL
-                    alt="AC Repairing"
-                    style={{
-                        marginTop:20,
-                        marginLeft: 'auto', // Center the image horizontally
-                        marginRight: 'auto',
-                        display: 'block',
-                        objectFit: 'cover',
-                        maxWidth: '80%', // Limit image size to its container
-                        maxHeight: '100px', // Maximum height set to 200px
-                    }}
-                />
-                <CardContent  sx={{ paddingBottom: '1px' }}>
-                <Typography gutterBottom variant="h6" component="div" style={{ fontSize: '0.8rem', color: '#333', textAlign: 'center', fontWeight: 'bold',  }}>
-                    Advisors
-                    <Typography gutterBottom variant="h6" component="div" style={{ fontSize: '0.8rem', color: '#333', fontWeight: 'bold', textAlign: 'center', marginLeft:'10px' }}>
-                    & Others
-                    <IconButton color="blue" sx={{ fontSize: 30, textAlign: 'center',}}>
-                    <ArrowRightAltSharpIcon />
-                </IconButton>
-                </Typography>
-                </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
-       
+       <Box sx={{ width: '100%', backgroundColor: '#baf1f0',marginTop:"25px",marginBottom:"88px" }}>
+           <div style={{ marginTop: "100px" }}>
+               <Typography variant="h6" align="center" fontWeight="bold" sx={{ marginTop: "20px", color: "#333", margin: "20px" }}>
+                   Our Services
+               </Typography>
+           </div>
+       <Grid container spacing={2}  direction="row">
+       <Grid  item xs={6} sm={6} md={3}>
+            <Card style={{ maxWidth: "128px", borderRadius: "10px", boxShadow: "0px 8px 24px rgba(0, 0, 0, 0.5)", margin: "auto", marginTop: "20px", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                <CardActionArea style={{ paddingBottom: "1px",width:"174px",paddingLeft:"1px" }}>
+                    <img
+                        src={appliancesinstall}
+                        alt="Appliances Repair"
+                        style={{ marginTop: "10px", marginLeft: "auto", marginRight: "auto", display: "block", objectFit: "cover", maxWidth: "80%", maxHeight: "100px" }}
+                    />
+                   <Typography variant="h6" component="div" style={{ fontSize: "0.67rem", color: "#333", textAlign: "center", fontWeight: "bold", marginTop: "10px", lineHeight: "0.8" }}>
+    Appliances Installation 
     
-   
-        </>
-    )
+</Typography>
+<div style={{ display: 'flex', alignItems: 'center',marginLeft:"20px",marginTop:"2px" }}>
+    <Typography variant="h6" style={{ fontSize: "0.67rem", color: "#333", textAlign: "center", fontWeight: "bold", lineHeight: "1", marginRight: '5px' }}>
+        & Repair
+    </Typography>
+
+    <IconButton style={{ height: '24px' }}>
+    <ArrowRightAltSharpIcon />
+</IconButton>
+
+</div>    
+                </CardActionArea>
+            </Card>
+           </Grid>
+           <Grid  item xs={6} sm={6} md={3}>
+            <Card style={{ maxWidth: "128px", borderRadius: "10px", boxShadow: "0px 8px 24px rgba(0, 0, 0, 0.5)", margin: "auto", marginTop: "20px", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                <CardActionArea style={{ paddingBottom: "1px",width:"174px",paddingLeft:"1px" }}>
+                    <img
+                        src={home}
+                        alt=""
+                        style={{ marginTop: "10px", marginLeft: "auto", marginRight: "auto", display: "block", objectFit: "cover", maxWidth: "80%", maxHeight: "100px" }}
+                    />
+                   <Typography variant="h6" component="div" style={{ fontSize: "0.67rem", color: "#333", textAlign: "center", fontWeight: "bold", marginTop: "10px", lineHeight: "0.8" }}>
+    Home Cleaning
+</Typography>
+<div style={{ display: 'flex', alignItems: 'center',marginLeft:"20px",marginTop:"2px" }}>
+    <Typography variant="h6" style={{ fontSize: "0.67rem", color: "#333", textAlign: "center", fontWeight: "bold", lineHeight: "1", marginRight: '5px' }}>
+        & Others
+    </Typography>
+
+    <IconButton style={{ height: '24px' }}>
+    <ArrowRightAltSharpIcon />
+</IconButton>
+
+</div>            
+                </CardActionArea>
+            </Card>
+            </Grid>
+            
+            <Grid  item xs={6} sm={6} md={3}>
+            <Card style={{ maxWidth: "125px", borderRadius: "10px", boxShadow: "0px 8px 24px rgba(0, 0, 0, 0.5)", margin: "auto", marginTop: "20px", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                <CardActionArea style={{ paddingBottom: "1px",width:"174px",paddingLeft:"1px" }}>
+                    <img
+                        src={fitnesstrack}
+                        alt="Appliances Repair"
+                        style={{ marginTop: "10px", marginLeft: "auto", marginRight: "auto", display: "block", objectFit: "cover", maxWidth: "80%", maxHeight: "100px" }}
+                    />
+                   <Typography variant="h6" component="div" style={{ fontSize: "0.67rem", color: "#333", textAlign: "center", fontWeight: "bold", marginTop: "10px", lineHeight: "0.8" }}>
+    Health
+</Typography>
+<div style={{ display: 'flex', alignItems: 'center',marginLeft:"20px",marginTop:"2px" }}>
+    <Typography variant="h6" style={{ fontSize: "0.67rem", color: "#333", textAlign: "center", fontWeight: "bold", lineHeight: "1", marginRight: '5px' }}>
+    & Fitness
+    
+    </Typography>
+
+    <IconButton style={{ height: '24px' }}>
+    <ArrowRightAltSharpIcon />
+</IconButton>
+
+</div>
+
+
+                   
+                    
+                </CardActionArea>
+            </Card>
+            </Grid>
+            <Grid  item xs={6} sm={6} md={3}>
+            <Card style={{ maxWidth: "125px", borderRadius: "10px", boxShadow: "0px 8px 24px rgba(0, 0, 0, 0.5)", margin: "auto", marginTop: "20px", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                <CardActionArea style={{ paddingBottom: "1px",width:"174px",paddingLeft:"1px" }}>
+                    <img
+                        src={rennovate}
+                        alt="Appliances Repair"
+                        style={{ marginTop: "10px", marginLeft: "auto", marginRight: "auto", display: "block", objectFit: "cover", maxWidth: "80%", maxHeight: "100px" }}
+                    />
+                   <Typography variant="h6" component="div" style={{ fontSize: "0.67rem", color: "#333", textAlign: "center", fontWeight: "bold", marginTop: "10px", lineHeight: "0.8" }}>
+    House Repair
+</Typography>
+<div style={{ display: 'flex', alignItems: 'center',marginLeft:"20px",marginTop:"2px" }}>
+    <Typography variant="h6" style={{ fontSize: "0.67rem", color: "#333", textAlign: "center", fontWeight: "bold", lineHeight: "1", marginRight: '5px' }}>
+        & Others
+    </Typography>
+
+    <IconButton style={{ height: '24px' }}>
+    <ArrowRightAltSharpIcon />
+</IconButton>
+
+</div>
+
+
+                   
+                    
+                </CardActionArea>
+            </Card>
+            </Grid>
+            <Grid  item xs={6} sm={6} md={3}>
+            <Card style={{ maxWidth: "125px", borderRadius: "10px", boxShadow: "0px 8px 24px rgba(0, 0, 0, 0.5)", margin: "auto", marginTop: "20px", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                <CardActionArea style={{ paddingBottom: "1px",width:"174px",paddingLeft:"1px" }}>
+                    <img
+                        src={eventplanner}
+                        alt="Appliances Repair"
+                        style={{ marginTop: "10px", marginLeft: "auto", marginRight: "auto", display: "block", objectFit: "cover", maxWidth: "80%", maxHeight: "100px" }}
+                    />
+                   <Typography variant="h6" component="div" style={{ fontSize: "0.67rem", color: "#333", textAlign: "center", fontWeight: "bold", marginTop: "10px", lineHeight: "0.8" }}>
+    Event Planning &
+    
+</Typography>
+<div style={{ display: 'flex', alignItems: 'center',marginLeft:"20px",marginTop:"2px" }}>
+    <Typography variant="h6" style={{ fontSize: "0.67rem", color: "#333", textAlign: "center", fontWeight: "bold", lineHeight: "1", marginRight: '5px' }}>
+        Managing
+    </Typography>
+
+    <IconButton style={{ height: '24px' }}>
+    <ArrowRightAltSharpIcon />
+</IconButton>
+
+</div>
+
+
+                   
+                    
+                </CardActionArea>
+            </Card>
+            </Grid>
+            <Grid  item xs={6} sm={6} md={3}>
+            <Card style={{ maxWidth: "125px", borderRadius: "10px", boxShadow: "0px 8px 24px rgba(0, 0, 0, 0.5)", margin: "auto", marginTop: "20px", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                <CardActionArea style={{ paddingBottom: "1px",width:"174px",paddingLeft:"1px" }}>
+                    <img
+                        src={movers}
+                        alt="Appliances Repair"
+                        style={{ marginTop: "10px", marginLeft: "auto", marginRight: "auto", display: "block", objectFit: "cover", maxWidth: "80%", maxHeight: "100px" }}
+                    />
+                   <Typography variant="h6" component="div" style={{ fontSize: "0.67rem", color: "#333", textAlign: "center", fontWeight: "bold", marginTop: "10px", lineHeight: "0.8" }}>
+    Packers
+    
+</Typography>
+<div style={{ display: 'flex', alignItems: 'center',marginLeft:"20px",marginTop:"2px" }}>
+    <Typography variant="h6" style={{ fontSize: "0.67rem", color: "#333", textAlign: "center", fontWeight: "bold", lineHeight: "1", marginRight: '5px' }}>
+        & Movers
+    </Typography>
+
+    <IconButton style={{ height: '24px' }}>
+    <ArrowRightAltSharpIcon />
+</IconButton>
+
+</div>
+
+
+                   
+                    
+                </CardActionArea>
+            </Card>
+            </Grid>
+            <Grid  item xs={6} sm={6} md={3}>
+            <Card style={{ maxWidth: "125px", borderRadius: "10px", boxShadow: "0px 8px 24px rgba(0, 0, 0, 0.5)", margin: "auto", marginTop: "20px", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                <CardActionArea style={{ paddingBottom: "1px",width:"174px",paddingLeft:"1px" }}>
+                    <img
+                        src={education}
+                        alt="Appliances Repair"
+                        style={{ marginTop: "10px", marginLeft: "auto", marginRight: "auto", display: "block", objectFit: "cover", maxWidth: "80%", maxHeight: "100px" }}
+                    />
+                   <Typography variant="h6" component="div" style={{ fontSize: "0.67rem", color: "#333", textAlign: "center", fontWeight: "bold", marginTop: "10px", lineHeight: "0.8" }}>
+    Education &
+    
+</Typography>
+<div style={{ display: 'flex', alignItems: 'center',marginLeft:"20px",marginTop:"2px" }}>
+    <Typography variant="h6" style={{ fontSize: "0.67rem", color: "#333", textAlign: "center", fontWeight: "bold", lineHeight: "1", marginRight: '5px' }}>
+        Skills 
+    </Typography>
+
+    <IconButton style={{ height: '24px' }}>
+    <ArrowRightAltSharpIcon />
+</IconButton>
+
+</div>
+
+
+                   
+                    
+                </CardActionArea>
+            </Card>
+            </Grid>
+            </Grid>
+            </Box>         
+           </>
+    );
 }
+
 export default Service;
